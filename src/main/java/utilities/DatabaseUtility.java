9c8d468d82cf02454051f2946304b2db7c53e16a
bheemesh <manu.bheemesh@gmail.com>
2023-07-23 01:24:19 +0530

    project for initial commit

---
+++ src/main/java/utilities/DatabaseUtility.java
@@ -0,0 +1,15 @@
+package utilities;
+
+public class DatabaseUtility {
+
+
+    public void connectSQLDatabase(String connectionUrl) {
+        System.out.println("connection in progress");
+    }
+
+    public void connectMySqlDatabase(String connectionUrl) {
+        System.out.println("connection in progress");
+    }
+
+
+}
